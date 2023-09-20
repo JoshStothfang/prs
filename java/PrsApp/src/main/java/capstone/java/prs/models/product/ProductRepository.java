3c847dcd0d1f05a06932556fcc1ab6fa6ce05a66
Josh Stothfang <josh@joshstothfang.com>
2023-09-20 14:01:15 -0400

    Added Product

---
+++ java/PrsApp/src/main/java/capstone/java/prs/models/product/ProductRepository.java
@@ -0,0 +1,7 @@
+package capstone.java.prs.models.product;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProductRepository extends CrudRepository<Product, Integer> {
+
+}
