d2e5b592de42817f3ef5949f0ea29cb5b82563e5
Josh Stothfang <josh@joshstothfang.com>
2023-09-20 10:05:12 -0400

    Initial Commit

---
+++ java/PrsApp/src/main/java/capstone/java/prs/PrsAppApplication.java
@@ -0,0 +1,13 @@
+package capstone.java.prs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrsAppApplication.class, args);
+	}
+
+}
---
+++ java/PrsApp/src/test/java/capstone/java/prs/PrsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package capstone.java.prs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
