9a514ec951f05f57be95eafe9b68298ade54486c
Josh Stothfang <josh@joshstothfang.com>
2023-09-20 11:47:09 -0400

    Added User

---
+++ java/PrsApp/src/main/java/capstone/java/prs/models/user/UserController.java
@@ -0,0 +1,100 @@
+package capstone.java.prs.models.user;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+	@Autowired
+	private UserRepository userRepo;
+	
+	@GetMapping
+	public ResponseEntity<Iterable<User>> getUsers() {
+		
+		Iterable<User> users = userRepo.findAll();
+		return new ResponseEntity<Iterable<User>>(users, HttpStatus.OK);
+	}
+	
+	@GetMapping("{id}")
+	public ResponseEntity<User> getUser(@PathVariable int id) {
+		
+		if (id <= 0 ) {
+			
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+		
+		Optional<User> user = userRepo.findById(id);
+		if (user.isEmpty()) {
+			
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		
+		return new ResponseEntity<User>(user.get(), HttpStatus.OK);
+	}
+	
+	
+	@GetMapping("{username}/{password}")
+	public ResponseEntity<User> loginUser(@PathVariable String username, @PathVariable String password) {
+		
+		Optional<User> user = userRepo.findByUsernameAndPassword(username, password);
+		if (user.isEmpty()) {
+			
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		
+		return new ResponseEntity<User>(user.get(), HttpStatus.OK);
+	}
+	
+	@PostMapping
+	public ResponseEntity<User> postUser(@RequestBody User user) {
+		
+		if (user.getId() != 0 ) {
+			
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+		
+		userRepo.save(user);
+		return new ResponseEntity<User>(user, HttpStatus.CREATED);
+	}
+	
+	@SuppressWarnings("rawtypes")
+	@PutMapping("{id}")
+	public ResponseEntity putUser(@PathVariable int id, @RequestBody User user) {
+		
+		if (id <= 0) {
+			
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}		
+		
+		userRepo.save(user);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	}
+	
+	@SuppressWarnings("rawtypes")
+	@DeleteMapping("{id}")
+	public ResponseEntity deleteUser(@PathVariable int id) {
+		
+		if (id <= 0) {
+			
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+		
+		userRepo.deleteById(id);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	}
+}
