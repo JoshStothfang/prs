525eac017d559b1c6c596b92a44b509411d2ecbe
Josh Stothfang <josh@joshstothfang.com>
2023-10-16 12:10:41 -0400

    Initial Commit

---
+++ angular/PrsApp/src/app/request-line/request-line.class.ts
@@ -0,0 +1,10 @@
+import { Product } from "../product/product.class";
+
+export class RequestLine {
+
+    id: number = 0;
+    requestId: number = 0;
+    productId: number = 0;
+    quantity: number = 1;
+    product?: Product;
+}
