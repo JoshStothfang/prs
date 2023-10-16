3b82a94d7d01703d0b0aa3d5081cdf79f132ac6c
Josh Stothfang <josh@joshstothfang.com>
2023-10-16 15:35:33 -0400

    Added User detail functionality

--- angular/PrsApp/src/app/user/user-detail/user-detail.component.ts
+++ angular/PrsApp/src/app/user/user-detail/user-detail.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { User } from '../user.class';
+import { SystemService } from 'src/app/core/system.service';
+import { UserService } from '../user.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user-detail',
@@ -7,4 +11,27 @@ import { Component } from '@angular/core';
 })
 export class UserDetailComponent {
 
+  user?: User;
+
+  constructor(
+    private sysSvc: SystemService,
+    private route: ActivatedRoute,
+    private userSvc: UserService
+  ) { }
+
+  ngOnInit(): void {
+
+    if(!this.sysSvc.loggedIn()) return;
+
+    let id = this.route.snapshot.params["id"];
+    this.userSvc.get(id).subscribe({
+      next: (res) => {
+        console.debug(res);
+        this.user = res;
+      },
+      error: (err) => {
+        console.error(err);
+      }
+    });
+  }
 }
