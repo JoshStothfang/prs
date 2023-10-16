43b3584f3740189afb949cac552461046f79e18d
Josh Stothfang <josh@joshstothfang.com>
2023-10-16 12:12:44 -0400

    Initial Commit

---
+++ angular/PrsApp/src/app/core/system.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { User } from '../user/user.class';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SystemService {
+
+  constructor() { }
+}
