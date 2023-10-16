e34d6705fd95fbcfba5f8336e7cd312d352446fc
Josh Stothfang <josh@joshstothfang.com>
2023-10-16 16:02:15 -0400

    Added User edit functionality

--- angular/PrsApp/src/app/user/user-edit/user-edit.component.ts
+++ angular/PrsApp/src/app/user/user-edit/user-edit.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { SystemService } from 'src/app/core/system.service';
+import { UserService } from '../user.service';
+import { User } from '../user.class';
 
 @Component({
   selector: 'app-user-edit',
@@ -7,4 +11,41 @@ import { Component } from '@angular/core';
 })
 export class UserEditComponent {
 
+  user?: User;
+
+  constructor(
+    private sysSvc: SystemService,
+    private route: ActivatedRoute,
+    private userSvc: UserService,
+    private router: Router
+  ) { }
+
+  send(): void {
+
+    this.userSvc.change(this.user!).subscribe({
+      next: (res) => {
+        console.debug(res);
+        this.router.navigateByUrl("/user/list");
+      },
+      error: (err) => {
+        console.error(err);
+      }
+    });
+  }
+
+  ngOnInit(): void {
+
+    if(!this.sysSvc.loggedIn()) return;
+
+    let id = this.route.snapshot.params["id"];
+    this.userSvc.get(id).subscribe({
+      next: (res) => {
+        console.debug(res);
+        this.user = res;
+      },
+      error: (err) => {
+        console.error(err);
+      }
+    });
+  }
 }
