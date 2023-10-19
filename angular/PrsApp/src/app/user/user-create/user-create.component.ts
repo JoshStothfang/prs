99ad23eaf9bfad5751bb376f0a52646f9e500687
Josh Stothfang <josh@joshstothfang.com>
2023-10-19 09:24:52 -0400

    Added Create user functionality

--- angular/PrsApp/src/app/user/user-create/user-create.component.ts
+++ angular/PrsApp/src/app/user/user-create/user-create.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { SystemService } from 'src/app/core/system.service';
+import { User } from '../user.class';
+import { UserService } from '../user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-create',
@@ -7,4 +11,28 @@ import { Component } from '@angular/core';
 })
 export class UserCreateComponent {
 
+  user: User = new User();
+
+  constructor(
+    private sysSvc: SystemService,
+    private userSvc: UserService,
+    private router: Router
+  ) { }
+  
+  send(): void {
+    this.userSvc.create(this.user).subscribe({
+      next: (res) => {
+        console.debug(res);
+        this.router.navigateByUrl("/user/list");
+      },
+      error: (err) => {
+        console.error(err);
+      }    
+    });
+  }
+
+  ngOnInit(): void {
+
+    if(!this.sysSvc.loggedIn()) return;
+  }
 }
