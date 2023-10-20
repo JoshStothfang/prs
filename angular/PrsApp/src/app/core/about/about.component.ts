37cdca98576345244d1503463be2f669052c1d74
Josh Stothfang <josh@joshstothfang.com>
2023-10-20 11:14:58 -0400

    Added menu to About component

--- angular/PrsApp/src/app/core/about/about.component.ts
+++ angular/PrsApp/src/app/core/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SystemService } from '../system.service';
 
 @Component({
   selector: 'app-about',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AboutComponent {
 
+  constructor(
+    private sysSvc: SystemService
+  ) { }
+
+  ngOnInit(): void {
+
+    if (!this.sysSvc.loggedIn()) return;
+  }
 }
